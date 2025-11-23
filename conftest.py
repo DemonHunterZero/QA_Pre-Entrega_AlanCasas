45ae4f4aadbc46ba0d2fd5fdb18259e851645b70
Zero <zero-swords@hotmail.com>
2025-11-23 15:30:18 -0300

    cambios

--- conftest.py
+++ conftest.py
@@ -5,7 +5,9 @@ from utils.utils import login, create_webdriver_chrome
 #Las funciones de este archivo son GLOBALES por llamarse conftest. No hace falta importarlas
 @pytest.fixture
 def chrome_driver():
+    #driver = webdriver.Chrome()
     driver = create_webdriver_chrome()
+    driver.implicitly_wait(10) #agregado (borrar sino funciona)
     yield driver
     driver.quit()
 
