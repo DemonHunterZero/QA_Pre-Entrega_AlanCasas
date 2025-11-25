be36e421368bb4130228981086bac4c99b1e2d9b
Zero <zero-swords@hotmail.com>
2025-11-24 22:19:27 -0300

    Cambios del 24/11

--- conftest.py
+++ conftest.py
@@ -1,17 +1,37 @@
 import pytest
 from selenium import webdriver
-from utils.utils import login, create_webdriver_chrome
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options # Importar Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from pages.login_page import LoginPage
 
 #Las funciones de este archivo son GLOBALES por llamarse conftest. No hace falta importarlas
 @pytest.fixture
 def chrome_driver():
-    #driver = webdriver.Chrome()
-    driver = create_webdriver_chrome()
+    
+    # Configurar las opciones de Chrome para evitar el popup de contraseña (SOLUCION CHAT GTP)
+    chrome_options = Options()
+    # Esta preferencia evita el cuadro de diálogo "Guardar contraseña" de Chrome
+    prefs = {
+        "credentials_enable_service": False,
+        "profile.password_manager_enabled": False,
+        "profile.default_content_setting_values.notifications": 2,
+        "profile.default_content_setting_values.popups": 0,
+        "profile.exit_type": "Normal",
+        "profile.exited_cleanly": True,
+        "profile.password_manager_leak_detection": False  # <--- ESTA ES LA CLAVE
+    }
+    chrome_options.add_experimental_option("prefs", prefs)
+    chrome_options.add_argument('--start-maximized') # Opcional: ventana maximizada
+
+    driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(10) #agregado (borrar sino funciona)
     yield driver
     driver.quit()
 
 @pytest.fixture
-def login_in_chrome(chrome_driver):
-    login(chrome_driver)
+def login_in_chrome(chrome_driver, user, password):
+    LoginPage(chrome_driver).abrir_pagina().login_completo(user,password)
     return chrome_driver
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,18 +1,31 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from pages.login_page import LoginPage
 import pytest
 import time
 
-#py -m pytest test_login.py -v para ejecutarlo como prueba
-#py -m pytest test_login.py -v --html=report.html --self-contained-html para ejecutarlo y realizar el reporte html
-def test_login_validation(login_in_chrome):
+#py -m pytest tests/test_login.py -v para ejecutarlo como prueba
+#py -m pytest tests/test_login.py -v --html=report.html --self-contained-html para ejecutarlo y realizar el reporte html
+@pytest.mark.parametrize("user, password, debe_funcionar",(
+        ("standard_user","secret_sauce", True),
+        ("locked_out_user","secret_sauce", False),
+        ("problem_user","secret_sauce", True),
+        ("performance_glitch_user","secret_sauce", True),
+        ("error_user","secret_sauce", True),
+        ("visual_user","secret_sauce", True)
+))
+def test_login_validation(login_in_chrome, user, password, debe_funcionar):
     try:
-        driver = login_in_chrome
+        driver = login_in_chrome #toma los parametros user y password implicitamente
         
-        #Validacion de url luego del login
-        assert '/inventory.html' in driver.current_url, "No se redirigio al inventario"
-        print("Login exitoso y validado")
+        if debe_funcionar:
+            #Validacion de url luego del login        
+            assert '/inventory.html' in driver.current_url, "No se redirigio al inventario"
+            print("Login exitoso y validado")
+        else:
+            mensaje_error = LoginPage(driver).obtener_error()
+            assert "Epic sadface" in mensaje_error, "El mensaje de error no se muestra correctamente"
         
     
     #Manejo de errores
